80fdd1dc4f6a0bb61dd40cd5a31fb17a14593919
Ubuntu <Daniele@dev-vm.x0eelvynvgzenlehyv50c1i5sg.ax.internal.cloudapp.net>
2019-12-19 22:58:21 +0000

    Add LALR and start on the parser

---
+++ src/ast.rs
@@ -0,0 +1,15 @@
+use std::str::FromStr;
+
+pub struct ProductionChain {
+    pub id: String,
+    pub owner: String,
+    pub blocks: Vec<ProductionBlock>,
+}
+
+pub struct ProductionBlock {
+    pub id: String,
+    pub name: String,
+    pub power: i32,
+    pub ingredients: Vec<(String, i32)>,
+    pub recipes: Vec<()>,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+#[macro_use]
+extern crate lalrpop_util;
+
+lalrpop_mod!(pub grammar); // synthesized by LALRPOP
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[test]
+fn test_grammar() {
+    assert!(grammar::TermParser::new().parse("22").is_ok());
+    assert!(grammar::TermParser::new().parse("(22)").is_ok());
+    assert!(grammar::TermParser::new().parse("((((22))))").is_ok());
+    assert!(grammar::TermParser::new().parse("((22)").is_err());
 }
